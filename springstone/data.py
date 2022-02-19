3f97701e87b6d536631711799f642647b0952c94
Brahim El Merbouh <elmerbouh@gmail.com>
2022-02-19 15:38:25 -0500

    add function to return missing dates from financial data

--- springstone/data.py
+++ springstone/data.py
@@ -1,5 +1,17 @@
 import yfinance as yfB
+import pandas as pd
+import numpy as np
+
 def get_data(ticker_name,start="2012-02-18",end="2022-02-19"):
     '''returns a DataFrame from yfinance'''
     data = yfB.download(ticker_name,start,end)
     return data
+
+def get_missing_dates(dataframe):
+    """Returns missing dates in the finance data
+       Input:
+           data: dataframe
+       Output: numpy array of dates"""
+    start_date = dataframe.index[0]
+    end_date = dataframe.index[dataframe.shape[0]-1]
+    return np.array(pd.bdate_range(start = start_date, end = end_date ).difference(dataframe.index))
