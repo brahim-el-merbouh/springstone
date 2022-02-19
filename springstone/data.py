e63f8fc3d69e419e86b846f7ea23a84174fd1066
Brahim El Merbouh <elmerbouh@gmail.com>
2022-02-19 16:37:43 -0500

    support weekend in get missing data function

--- springstone/data.py
+++ springstone/data.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 import yfinance as yfB
 import pandas as pd
 import numpy as np
@@ -7,11 +8,14 @@ def get_data(ticker_name,start="2012-02-18",end="2022-02-19"):
     data = yfB.download(ticker_name,start,end)
     return data
 
-def get_missing_dates(dataframe):
+def get_missing_dates(dataframe, include_weekends=False):
     """Returns missing dates in the finance data
        Input:
            data: dataframe
        Output: numpy array of dates"""
     start_date = dataframe.index[0]
     end_date = dataframe.index[dataframe.shape[0]-1]
-    return np.array(pd.bdate_range(start = start_date, end = end_date ).difference(dataframe.index))
+    if include_weekends:
+        return np.array(pd.date_range(start = start_date, end = end_date ).difference(dataframe.index))
+    else:
+        return np.array(pd.bdate_range(start = start_date, end = end_date ).difference(dataframe.index))
