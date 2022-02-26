322764c7a909856c6147817f31579df92c78240d
Brahim El Merbouh <elmerbouh@gmail.com>
2022-02-26 14:41:09 -0500

    some work done!

--- springstone/trainer.py
+++ springstone/trainer.py
@@ -1,9 +1,12 @@
 from pickle import NONE
 import pandas as pd
 from sklearn.pipeline import Pipeline
-#from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransformer
 #from sklearn.compose import ColumnTransformer
 from sklearn.metrics import mean_absolute_error
+from utils import prophet_preprocessing
+from data import get_data, create_df_for_prophet
+from params import PROPHET_COLUMN,PROPHET_PERIOD
 from prophet import Prophet
 
 class Trainer():
@@ -26,11 +29,13 @@ class Trainer():
         if isinstance(self.model, Prophet):
             self.pipeline = self.get_prophet_pipeline()
     def get_prophet_pipeline(self):
-        pipe = Pipeline([
-            ('prophet_model', Prophet(self.non_business_days))
+        pipe_ph = Pipeline([
+            ('prophet_preproc', FunctionTransformer(lambda df: prophet_preprocessing(df,PROPHET_COLUMN))),
+            ('prophet_model', Prophet(holidays=self.non_business_days))
         ])
     def get_RNN_pipeline(self):
-        
+        pass
+
     def compute_performance_metric(self, y_true, y_pred):
         if isinstance(self.model, Prophet):
             return mean_absolute_error(y_true, y_pred)
@@ -42,3 +47,18 @@ class Trainer():
         y_pred = self.pipeline.predict(X_test)
         rmse = self.compute_performance_metric(y_test, y_pred)
         return rmse
+
+if __name__ == "__main__":
+    # Get and clean data
+    df = get_data('TSLA')
+    df_prophet = clean_data(df)
+    y = df["fare_amount"]
+    X = df.drop("fare_amount", axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # Train and save model, locally and
+    trainer = Trainer(X=X_train, y=y_train)
+    trainer.set_experiment_name('xp2')
+    trainer.run()
+    rmse = trainer.evaluate(X_test, y_test)
+    print(f"rmse: {rmse}")
+    trainer.save_model()
